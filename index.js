dd706c8dffa6a308a1637fdf530e0039fcad08a6
Nikaosales <nicolassalesdefaria2000@gmail.com>
2023-11-14 17:59:58 -0300

    seleção de dados individuais

--- index.js
+++ index.js
@@ -1,76 +1,94 @@
-const express = require("express")
+const { Console } = require("console");
+const express = require("express");
 const exphbs = require("express-handlebars");
 const { request } = require("http");
-const mysql = require ("mysql2")
+const mysql = require("mysql2");
 
-const app = express()
+const app = express();
 
-app.engine("handlebars", exphbs.engine())
-app.set("view engine", "handlebars")
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
-app.use (express.static("public"))
+app.use(express.static("public"));
 
 app.use(express.urlencoded({
     extended: true
-}))
+}));
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/register/save", (req, res)=>{
-    const { title, pageqty} = req.body
+app.post("/register/save", (req, res) => {
+    const { title, pageqty } = req.body;
 
-    const query = 
-    `INSERT INTO books (title, pageqty)
+    const query =
+        `INSERT INTO books (title, pageqty)
     VALUES (' ${title}', ' ${pageqty}')
-    `
+    `;
     conn.query(query, (error) => {
-        if (error){
-            console.log(error)
-            return
+        if (error) {
+            console.log(error);
+            return;
         }
 
-        res.redirect("/")
-    })
-})
+        res.redirect("/");
+    });
+});
 
-app.get("/register", (req, res) =>{
-    res.render ("register")
-})
+app.get("/register", (req, res) => {
+    res.render("register");
+});
 
-app.get('/', (req, res) =>{
-     const sql = 'SELECT * FROM books'
+app.get("/book/:id", (req, res) => {
+    const id = req.params.id;
 
-     conn.query(sql, (error, data) => {
-        if (error){
-            return console.log (error)
+    const sql = `
+    SELECT * FROM books
+    WHERE id=${id}`;
+
+    conn.query(sql, (error, data) => {
+        if (error) {
+            return Console.log(error);
+        }
+        const book = data[0];
+
+        res.render("book", { book });
+    });
+});
+
+app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM books';
+
+    conn.query(sql, (error, data) => {
+        if (error) {
+            return console.log(error);
         }
 
-        const books = data
-      
-        res.render("home", {books})
-     })
+        const books = data;
 
-   
-})
+        res.render("home", { books });
+    });
+
+
+});
 
 const conn = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     user: "root",
     password: "Vingadores1@",
     database: "nodemysql",
     port: 3306
 
-})
+});
 
-conn.connect((error)=>{
-    if (error){
-        console.log(error)
-        return
+conn.connect((error) => {
+    if (error) {
+        console.log(error);
+        return;
     }
 
-    console.log("Conectado ao mysql!")
+    console.log("Conectado ao mysql!");
 
-    app.listen(3000, ()=>{
-        console.log("servidor rodando na porta 3000!")
-    })
-})
+    app.listen(3000, () => {
+        console.log("servidor rodando na porta 3000!");
+    });
+});
